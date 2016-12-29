501492eacf3a2c09a1a2ad3ca079f7bc77d60925
MarioUnoDueTreQuattro <andreagi0@yahoo.it>
2016-12-30 00:15:39 +0100

    c

--- app/src/main/java/com/thepriest/andrea/apppicker/MainActivity.java
+++ app/src/main/java/com/thepriest/andrea/apppicker/MainActivity.java
@@ -1,7 +1,9 @@
 package com.thepriest.andrea.apppicker;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
@@ -12,7 +14,11 @@ import android.widget.Toast;
 
 import org.apache.http.HttpVersion;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static android.content.pm.PackageManager.MATCH_ALL;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -50,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
         }
         else Log.d(TAG, "onCreate: NO Intent.ACTION_SEND");
 */
+/*
+        launchIntent();
+        if (true) return;
+*/
         Intent intent = this.getIntent();
         Log.d(TAG, "onCreate: " + intent.toString());
         String urlText = intent.getDataString();
@@ -58,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         Uri uri = Uri.parse(urlText);
         if (uri != null) {
             Intent i = null;
-            if (intent.getAction().equalsIgnoreCase(Intent.ACTION_PICK)||intent.getAction().equalsIgnoreCase("android.intent.action.RINGTONE_PICKER") )
+            if (intent.getAction().equalsIgnoreCase(Intent.ACTION_PICK) || intent.getAction().equalsIgnoreCase("android.intent.action.RINGTONE_PICKER"))
                 i = new Intent("android.intent.action.RINGTONE_PICKER", uri);
             else i = new Intent(Intent.ACTION_VIEW, uri);
 //            Intent i = Intent.makeMainSelectorActivity(Intent.ACTION_MAIN,
@@ -87,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
             //List<ResolveInfo> allActivities = getApplicationContext().getPackageManager().queryIntentActivities(intent, 0);
             // intent.setSelector(i);
             Intent newInt = null;
-            if (intent.getAction().equalsIgnoreCase(Intent.ACTION_PICK )|| intent.getAction().equalsIgnoreCase("android.intent.action.RINGTONE_PICKER"))
+            if (intent.getAction().equalsIgnoreCase(Intent.ACTION_PICK) || intent.getAction().equalsIgnoreCase("android.intent.action.RINGTONE_PICKER"))
                 newInt = Intent.createChooser(i,/* i.getAction() + " " +*/ i.getType());
             else newInt = Intent.createChooser(i, urlText);
             //LauncherActivity.launchBrowser(newInt);
@@ -125,10 +135,10 @@ public class MainActivity extends AppCompatActivity {
 
     private Intent cleanIntent(Intent passedIntent) {
         //Intent myIntent= passedIntent.getSelector();
-        List<ResolveInfo> launchables = getPackageManager().queryIntentActivities(passedIntent, PackageManager.MATCH_ALL);
+        List<ResolveInfo> launchables = getPackageManager().queryIntentActivities(passedIntent, MATCH_ALL);
         for (ResolveInfo resInfo : launchables) {
             Log.d(TAG, "cleanIntent: " + resInfo.resolvePackageName);
-            if (resInfo.resolvePackageName!=null) {
+            if (resInfo.resolvePackageName != null) {
                 if (resInfo.resolvePackageName.equalsIgnoreCase("com.thepriest.andrea.apppicker")) {
                     Log.d(TAG, "cleanIntent: cleaning...");
                     launchables.remove(resInfo);
@@ -156,6 +166,86 @@ public class MainActivity extends AppCompatActivity {
             }
 
             return Uri.parse(url);
+        }
+    }
+
+    private void startIntentFromResolveInfo(ResolveInfo resolveInfo) {
+//        if (this.autoStart != null) {
+//            this.autoStart.cancel();
+//        }
+        ActivityInfo ai = resolveInfo.activityInfo;
+        Intent intent = new Intent(getIntent());
+        intent.addFlags(50331648);
+        intent.setComponent(new ComponentName(ai.applicationInfo.packageName, ai.name));
+        try {
+            startActivity(intent);
+        } catch (SecurityException e) {
+            intent.setComponent(null);
+            intent.setPackage(ai.packageName);
+            startActivity(intent);
+        }
+        finish();
+    }
+
+    private Intent makeMyIntent() {
+        Intent intent = new Intent(getIntent());
+        intent.setComponent(null);
+        intent.setFlags(intent.getFlags() & -8388609);
+        return intent;
+    }
+
+    private void launchIntent() {
+        Intent launchIntent = getIntent();
+        // this.original = makeMyIntent();
+        Intent intent = new Intent("android.intent.action.VIEW");
+        String urlText = launchIntent.getDataString();
+        //Uri uri = findUrlInString(urlText);
+        if (urlText == null) urlText = "";
+        Uri uri = Uri.parse(urlText);
+        if (uri != null) {
+            Log.d(TAG, "launchIntent urlText: " + urlText);
+            intent.setDataAndType(uri, "text/html");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+            intent.setDataAndType(launchIntent.getData(), launchIntent.getType());
+/*
+        int id = -1;
+        PackageManager packageManager = getPackageManager();
+        try {
+            id = getPackageManager().getActivityInfo(getComponentName(), PackageManager.MATCH_ALL).metaData.getInt("id", -1);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+*/
+            //this.item = getHandleItem(id);
+            PackageManager packageManager = getPackageManager();
+            intent=cleanIntent(intent);
+            List<ResolveInfo> resInfo = packageManager.queryIntentActivities(intent, MATCH_ALL);
+            Log.d(TAG, "launchIntent resInfo.size(): " + resInfo.size());
+            for (ResolveInfo ress : resInfo){
+                Log.d(TAG, "launchIntent: " + ress.toString());
+            }
+
+            if (resInfo.size() == 1) {
+                Log.d(TAG, "launchIntent: " + resInfo.get(0).toString());
+                Toast.makeText(this, "No application found to open the selected file", Toast.LENGTH_LONG).show();
+                finish();
+            }
+            if (resInfo.size() == 2) {
+                Log.d(TAG, "launchIntent: resInfo.size() == 2" + resInfo.get(1).toString());
+                ResolveInfo resolveInfo = (ResolveInfo) resInfo.get(0);
+                if (resolveInfo.activityInfo.packageName.equals(getPackageName())) {
+                    startIntentFromResolveInfo((ResolveInfo) resInfo.get(1));
+                } else {
+                    startIntentFromResolveInfo(resolveInfo);
+                }
+                finish();
+            }
+                    Intent chooserIntent=Intent.createChooser(intent,"ggggggggg");
+                    //chooserIntent=cleanIntent(chooserIntent);
+                    chooserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+
+                    startActivity(chooserIntent);
+                    return;
         }
     }
 }
